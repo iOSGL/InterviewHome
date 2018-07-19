4815def67b641d6928f914fb0fa109a3607771d0
geng199200 <1247759034@qq.com>
2018-07-19 11:58:49 +0800

    跳转到题详情

---
+++ src/View/Skill/QuestionDetail.vue
@@ -0,0 +1,54 @@
+<template>
+    <div v-bind:class="['wrapper', ipx?'ipx':'']">
+        <navigation-header title="详情" :leftBtn="leftBtn" @leftAction="back"></navigation-header>
+    </div>
+</template>
+
+<script>
+    import util from '../../util.js';
+    import header from '../../components/Header.vue';
+    var navigator = weex.requireModule('navigator');
+    export default {
+        name: "QuestionDetail",
+        data () {
+            return {
+                ipx: '',
+                leftBtn: {
+                    name:'e609;'
+                },
+                questionID: ''
+            }
+        },
+        components: {
+            'navigation-header': header,
+        },
+        created () {
+            util.initIconFont();
+            this.ipx = util.isIpx();
+            this.questionID = util.getUrlSearch(weex.config.bundleUrl, 'questionID');
+            console.log(this.questionID);
+        },
+        methods: {
+            back () {
+                navigator.pop({
+                    animated: 'true',
+                })
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+    .wrapper{
+        position: absolute;
+        left: 0;
+        right:0;
+        bottom: 0;
+        top:0;
+    }
+    .w-ipx {
+        margin-top: 40px;
+        margin-bottom:0px;
+    }
+</style>
--- src/View/Skill/QuestionList.vue
+++ src/View/Skill/QuestionList.vue
@@ -1,6 +1,16 @@
 <template>
-    <div class="wrapper">
+    <div v-bind:class="['wrapper', ipx?'w-ipx':'']">
         <navigation-header :title="title" :leftBtn="leftBtn" @leftAction="back"></navigation-header>
+        <list class="list" >
+            <template v-for="(obj, index) in dataArray">
+                <cell class="box" v-on:click="didSelectRow(obj)">
+                    <div class="box contentView" >
+                        <text class="no-text">{{'第' + (index+1) + '题' }}</text>
+                        <text class="tlt-text">{{obj.questionTitle}}</text>
+                    </div>
+                </cell>
+            </template>
+        </list>
     </div>
 </template>
 
@@ -17,7 +27,10 @@
                     name: 'e609;'
                 },
                 title:'',
-                pageID:''
+                pageID:'',
+                dataArray: [],
+                ipx: ''
+
             }
         },
         components: {
@@ -25,21 +38,32 @@
         },
         created () {
             util.initIconFont();
+            this.ipx = util.isIpx();
             storage.getItem('params', event => {
                 this.title = event.data;
                 storage.removeItem('params');
             })
             this.pageID = util.getUrlSearch(weex.config.bundleUrl, 'pageID');
             util.POST('/skill/questionList', {groupId:this.pageID}).then(res => {
-                console.log('result' + res.data.data);
+                this.dataArray =  res.data.data;
             }).catch(res => {
-                console.log('faile' +res.data.data);
+                this.dataArray =  res.data.data;
             })
         },
         methods: {
             back () {
                 navigator.pop({
                     animated: 'true'
+                })
+            },
+            didSelectRow (obj) {
+                let path = 'QuestionDetail.js?questionID=' + obj._id;
+                const url = weex.config.bundleUrl;
+                navigator.push({
+                    url: util.setBundleUrl(url, path),
+                    animation: 'true'
+                }, function (callBack) {
+
                 })
             }
         }
@@ -63,4 +87,42 @@
         bottom: 0;
         top:0;
     }
+    .w-ipx {
+        margin-top: 40px;
+        margin-bottom:0px;
+    }
+    .list {
+        margin-top: 113px;
+        margin-bottom: 0px;
+    }
+    .box {
+        height: 110px;
+    }
+    .contentView {
+        flex-direction: column;
+        border-bottom-width: 1px;
+        border-bottom-color: #eee;
+        border-bottom-style: solid;
+    }
+    .no-text {
+        font-size: 25px;
+        margin-left: 30px;
+        margin-top: 10px;
+        flex-wrap: nowrap;
+        color: #B0B0B0;
+        /*font-weight: bold;*/
+    }
+    .tlt-text {
+        font-size: 30px;
+        margin-left: 30px;
+        margin-top: 10px;
+        margin-right: 5px;
+        flex-wrap: nowrap;
+        color: #696969;
+        overflow:hidden;
+        text-overflow:ellipsis;
+        white-space:nowrap;
+        lines: 1;
+    }
+
 </style>
