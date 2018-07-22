916bce93cd2b28d3665b64dd9b5bdca4293db018
geng199200 <1247759034@qq.com>
2018-07-22 14:34:23 +0800

    commit code

--- src/View/Skill/QuestionDetail.vue
+++ src/View/Skill/QuestionDetail.vue
@@ -3,6 +3,8 @@
         <navigation-header title="详情" :leftBtn="leftBtn" @leftAction="back"></navigation-header>
         <top :leftText="className" :collection="collection" :rightTetx="numText"></top>
         <ques-detail :title="title" :content="content"></ques-detail>
+        <bottom @up="upAction" @down="nextAction"></bottom>
+
     </div>
 </template>
 
@@ -11,7 +13,9 @@
     import header from '../../components/Header.vue';
     import detailTop from '../../components/DetailTop.vue'
     import detailContent from '../../components/DetailContent.vue'
+    import detailBottom from '../../components/DetailBottom.vue'
     var navigator = weex.requireModule('navigator');
+    var modal = weex.requireModule('modal')
     export default {
         name: "QuestionDetail",
         data () {
@@ -36,6 +40,7 @@
             'navigation-header': header,
             'top': detailTop,
             'ques-detail': detailContent,
+            'bottom': detailBottom
         },
         created () {
             util.initIconFont();
@@ -45,21 +50,54 @@
                 isSingle: true,
                 groupId: this.questionID,
             }
-            util.POST('/skill/questionDetail', param).then(res => {
-                this.dataGroup = res.data.data[0];
-                // this.className = this.dataGroup.className;
-                // this.collection = this.dataGroup.isCollection;
-                this.numText = '1/200';
-                this.title = this.dataGroup.questionTitle;
-                this.content = '        ' + this.dataGroup.answer;
-            }).catch( res => {
-                console.log('失败'  + res);
-            })
+            this.requestData('/skill/questionDetail',param, '');
+
         },
         methods: {
             back () {
                 navigator.pop({
                     animated: 'true',
+                })
+            },
+            upAction () {
+                let param = {
+                    type: 'prev',
+                    id: this.questionID,
+                }
+                this.requestData('/skill/paging',param, 'upAction');
+            },
+            nextAction () {
+                let param = {
+                    type: 'next',
+                    id: this.questionID,
+                }
+                this.requestData('/skill/paging',param, 'nextAction');
+            },
+            requestData (path,param, type) {
+                util.POST(path, param).then(res => {
+                    this.dataGroup = res.data.data[0];
+                    if (type == 'nextAction' && res.data.data.length == 0)  {
+                          modal.toast({
+                              message: '已经是最后一题了',
+                              duration: 0.3
+                          })
+                          return;
+                      } else if (type == 'upAction' && res.data.data.length == 0) {
+                          modal.toast({
+                              message: '已经是第一题了',
+                              duration: 0.3
+                          })
+                          return;
+                      }
+
+                    // this.className = this.dataGroup.className;
+                    // this.collection = this.dataGroup.isCollection;
+                    this.numText = '1/200';
+                    this.title = this.dataGroup.questionTitle;
+                    this.content = '        ' + this.dataGroup.answer;
+                    this.questionID = this.dataGroup._id;
+                }).catch( res => {
+                    console.log('失败'  + res);
                 })
             }
         }
---
+++ src/components/DetailBottom.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="wrapper">
+        <div class="text-dev">
+            <text class="up-text" v-on:click="upQuestion">{{text1}}</text>
+            <text class="down-text" v-on:click="downQuestion">{{text2}}</text>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DetailBottom",
+        data () {
+            return {
+                text1: '上一题',
+                text2: '下一题'
+            }
+        },
+        methods: {
+            upQuestion () {
+                this.$emit('up')
+            },
+            downQuestion () {
+                this.$emit('down')
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper {
+        height: 120px;
+        background-color: #FAB340;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0px;
+        justify-content: center;
+        padding-left: 100px;
+        padding-right: 100px;
+    }
+    .text-dev {
+        flex-direction: row;
+        justify-content: space-around;
+    }
+    .up-text {
+        color: white;
+        font-size: 30px;
+    }
+    .down-text {
+        color: white;
+        font-size: 30px;
+    }
+</style>
