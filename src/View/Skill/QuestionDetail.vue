62a03dc17621a8366e762f7025256ee7629db98f
geng199200 <1247759034@qq.com>
2018-07-22 10:12:42 +0800

    修改试题详情

--- src/View/Skill/QuestionDetail.vue
+++ src/View/Skill/QuestionDetail.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-bind:class="['wrapper', ipx?'ipx':'']">
         <navigation-header title="详情" :leftBtn="leftBtn" @leftAction="back"></navigation-header>
+        <top></top>
     </div>
 </template>
 
 <script>
     import util from '../../util.js';
     import header from '../../components/Header.vue';
+    import detailTop from '../../components/DetailTop.vue'
     var navigator = weex.requireModule('navigator');
     export default {
         name: "QuestionDetail",
@@ -16,17 +18,29 @@
                 leftBtn: {
                     name:'e609;'
                 },
-                questionID: ''
+                questionID: '',
+                dataGroup: {
+
+                }
             }
         },
         components: {
             'navigation-header': header,
+            'top': detailTop
         },
         created () {
             util.initIconFont();
             this.ipx = util.isIpx();
             this.questionID = util.getUrlSearch(weex.config.bundleUrl, 'questionID');
-            console.log(this.questionID);
+            let param = {
+                isSingle: true,
+                groupId: this.questionID,
+            }
+            util.POST('/skill/questionDetail', param).then(res => {
+                this.dataGroup = res.data.data[0];
+            }).catch( res => {
+                console.log('失败'  + res);
+            })
         },
         methods: {
             back () {
---
+++ src/components/DetailTop.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DetailTop"
+    }
+</script>
+
+<style scoped>
+    .wrapper {
+        margin-top: 113px;
+        height: 100px;
+        background-color: #eee;
+    }
+
+</style>
